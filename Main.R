e47d0cea0c284c32584def686f558a76c39b2c33
Caroline <caroline.silvestre@live.com>
2018-07-28 18:05:11 +0100

    stl works
    but data doesn't look right

--- Main.R
+++ Main.R
@@ -3,7 +3,7 @@
 
 #loading sales data with dates and formatting the date field as date in R
 library(readr)
-SalesData <- read_csv("AllHistoricalData.csv")
+SalesData <-  as.data.frame(read_csv("AllHistoricalData.csv")) 
 dates <- as.Date(SalesData$Date)
 SalesData$Date <- dates
 
@@ -16,6 +16,18 @@ WeatherData$DATE <- weatherdates
 #need to transform sales data into a ts (time series) object
 
 library(xts)
+#DailySales <- xts(SalesData$Date, order.by = SalesData$Date) didn't work as expected
+test <- ts(SalesData$`Tickets Sold`, start = 1, frequency = 7)
+is.ts(test)
+test
 
-fit <- stl(SalesData, t.window=15, s.window="periodic", robust=TRUE)
+
+#fitting a STL decomposition to the sales data
+require(graphics)
+
+fit <- stl(test, s.window=12, robust=TRUE)
 plot(fit)
+
+
+ts(1:40, frequency = 1, start = 1) 
+print( ts(1:40, frequency = 7, start = c(1959)), calendar = TRUE)
