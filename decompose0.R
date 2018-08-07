c58432071375b2eceb7745a9d4b0af436b911682
Walter Eaves <walter.d.eaves@gmail.com>
2018-08-07 19:22:04 +0100

    decomposed

---
+++ decompose0.R
@@ -0,0 +1,49 @@
+## weaves
+##
+## Observed change in claims after 2016-07
+## 
+
+rm(list=ls())
+gc()
+
+## For footways
+
+library(dplyr)
+library(reshape2)
+library(lubridate)
+library(ggplot2)
+
+library(readr)
+df1 <- read_csv("sales0-M.csv", col_types = cols(dt0 = col_date(format = "%Y-%m")))
+View(df1)
+
+## reshape this
+
+df1m <- melt(df1, id.vars = c("dt0"))
+
+p0 <- ggplot(data = df1m, aes(dt0, log(value), group=variable, colour=variable) ) + 
+  geom_line(size = 0.8) + theme_bw()
+
+df2 <- df1m[ df1m$variable == "sales", ]
+
+data1 <- df2 %>% select(dt0, value) %>% 
+  mutate(yr0=as.integer(year(dt0)), month0=as.integer(month(dt0))) %>%
+  select(dt0, yr0, month0, value)
+
+data2 <- ts(data1$value, 
+            start=c(data1$yr0[1], data1$month0[1]), 
+            end=c(data1$yr0[nrow(data1)], data1$month0[nrow(data1)]), 
+            frequency=12)
+
+sales0 <- decompose(data2)
+
+jpeg(filename=paste("decompose", "-%03d.jpeg", sep=""), 
+     width=1024, height=768)
+
+print(p0)
+
+plot(sales0)
+title("Sales")
+
+x0 <- lapply(dev.list(), function(x) dev.off(x))
+
