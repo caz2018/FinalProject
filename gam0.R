5299091f338850ef991b94cccd548d9c677db718
Walter Eaves <walter.d.eaves@gmail.com>
2018-09-19 11:16:18 +0100

    added GAM models

--- gam0.R
+++ gam0.R
@@ -14,15 +14,20 @@ library(ggplot2)
 library(grid)
 library(caret)
 library(reshape2)
+library(gtools)
 
-if(length(commandArgs(trailingOnly=TRUE)) <= 0) {
-    x.args <- c("src/sales0-M.csv")
+x.plots <- list()
+
+x.args <- c("src/sales0-M.csv", "sales", "cp")
+
+if(length(commandArgs(trailingOnly=TRUE)) > 0) {
+    x.args <- commandArgs(trailingOnly=TRUE)
 }
 
 ## This loads the data and performs adjustments.
 source("gam00.R")
 
-p1 <- ggplot(data0, aes(dt0, log(sales))) +
+p1 <- ggplot(data0, aes(dt1, log(sales))) +
   geom_line() +
   theme(panel.border = element_blank(), panel.background = element_blank(), panel.grid.minor = element_line(colour = "grey90"),
         panel.grid.major = element_line(colour = "grey90"), panel.grid.major.x = element_line(colour = "grey90"),
@@ -30,13 +35,11 @@ p1 <- ggplot(data0, aes(dt0, log(sales))) +
         axis.title = element_text(size = 12, face = "bold")) +
     labs(x = "Date", y = "count")
 
-print(p1)
-
-x.plots <- list()
+x.plots[['log-sales']] <- p1
 
 ## Scaled data
 c0 <- colnames(data0)
-idx <- grepl("s\\.sales$|dt1|s\\..+1", c0)
+idx <- grepl("s\\.growth|s\\.sales$|dt1|s\\..+1", c0)
 
 data1 <- data0[, c0[idx]]
 
@@ -45,20 +48,190 @@ data1 <- melt(data1, id.vars="dt1")
 p1 <- ggplot(data = data1, aes(dt1, value, group = variable, colour = variable)) +
     geom_line(size = 0.8) +
     theme_bw() +
-    labs(x = "date", y = data1$variable,
+    labs(x = "date", y = "deviations",
          title = sprintf("Scaled and centred except %s", 's.sales'))
 
 x.plots[['scaled']] <- p1
+
+## Pre-process
+
+data1 <- data0[, !colnames(data0) %in% ltp[['xclde']] ]
+
+## theory: GLM: Iteratively Re-weighted Least Squares (IRLS) theory:
+
+## GAM: Penalized Iteratively Re-weighted Least Squares (P-IRLS)
+##
+## k -> ## (knots) is upper boundery for EDF
+##
+## how smooth fitted value will be (more knots more overfit (under
+## smoothed), less more smooth)
+
+## bs -> basis function..type of smoothing function
+##
+## dimension can be fixed by fx = TRUE EDF (trace of influence matrix)
+## and lambda (smoothing factor) is estimated (tuned) by GCV, UBRE or REML.
+
+## We will use the default GCV (Generalized Cross Validation) (more in
+## Woods) basis function.
+
+## I will use "cr" - cubic regression spline or "ps", which
+## is P-spline (more in Woods).
+
+## More options: "cc" cyclic cubic regression spline (good too with
+## our problem), default is "tp" thin plane spline family.
+
+## How response is fitted -> gaussian, log_norm, gamma, log_gamma is
+## our possibilities, but gaussian is most variable in practice
+## because gamma distibution must have only positive values gamm -
+## possibility to add autocorrelation for errors
+
+## Families and weights
+
+family0 <- gaussian
+family0 <- ziP                          # locks up
+family0 <- Tweedie                      # needs setting
+
+family0 <- poisson(link="log")          # rank 3
+# family0 <- log_norm                     # identical to gaussian with log?
+family0 <- gaussian(link="log")         # best
+
+## Active inputs only
+
+data1 <- data0[, !colnames(data0) %in% ltp[['xclde']] ]
+
+## For non-logged outcome use a logged link method
+if (ltp[['outcomen']] == "sales") {
+    family0 <- gaussian(link="log")         # best
+}
+
+## Use a logged and scaled figure with a Poisson
+if (ltp[['outcomen']] == "s.sales1") {
+    s0 <- data1[["s.sales1"]]
+    m0 <- min(s0)
+    if (m0 < 0) {
+        data1[["s.sales1"]] <- abs(m0) + s0
+    }
+    family0 <- poisson()
+}
+
+## Rename the outcome to something generic.
+
+c0 <- colnames(data1)
+c0[c0 == ltp[["outcomen"]] ] <- "value"
+colnames(data1) <- c0
+
+## Store GAMs in here
+
+gs <- list()
+
+## Weirdly, the month doesn't help. It seems to deduce it from the weather
+## I only use it for the tensor as the basis
+
+## GAM 1
+
+## It doesn't like the deltas or af. af is dropped as a NZV.
+
+## Check
+## ftres <- ltp[['inputn']]
+## ftres <- head(ftres, 3)
+
+x.bag0 <- ltp[['inputn']]
+
+n0 <- length(x.bag0)
+
+ftres1 <- combinations(n=n0, r=n0-1, v=x.bag0)
+ftres2 <- lapply(1:nrow(ftres1), function(x) ftres1[x,])
+
+force0 <- TRUE
+for (ftres in ftres2) {
+
+    print(ftres)
+    descrs <- paste(sapply(ftres, ltp.paste0), collapse=" + ")
+
+    fmla <- as.formula(paste("value ~ ", descrs))
+    tag <- paste("ps:", ftres, collapse=", ")
+
+    gam0 <- gam(fmla,
+                weights = wts,
+                data = data1,
+                family = family0)
+
+    gam0[["name0"]] <- tag
+    gam0[["ftres"]] <- unique(ftres)
+    gs[[tag]] <- gam0
+
+    ltp.gamS(gam0, force0=force0)       # just to start with
+    force0 <- FALSE
+
+    summary(gam0)$r.sq
+}
+
+## Cubic splines
+
+x.bag0 <- setdiff(ltp[['inputn']], c("dh0")) # can't use dh0
+
+n0 <- length(x.bag0)
+
+ftres1 <- combinations(n=n0, r=n0, v=x.bag0)
+ftres2 <- lapply(1:nrow(ftres1), function(x) ftres1[x,])
+
+for (ftres in ftres2) {
+
+    print(ftres)
+    descrs <- paste(sapply(ftres, function(x) ltp.paste0(x, bs0=x.args[3])), collapse=" + ")
+
+    fmla <- as.formula(paste("value ~ ", descrs))
+    tag <- paste("ps:", ftres, collapse=", ")
+
+    gam0 <- gam(fmla,
+                weights = wts,
+                data = data1,
+                family = family0)
+
+    gam0[["name0"]] <- tag
+    gam0[["ftres"]] <- unique(ftres)
+    gs[[tag]] <- gam0
+
+    ltp.gamS(gam0, force0=force0)       # just to start with
+    force0 <- FALSE
+
+    summary(gam0)$r.sq
+}
+
+## Ad-hoc: using a tensor 
+
+ftres <- c("mm0", "growth", "fx1", "weather")
+
+fmla <- "value ~ te(growth, fx1) + s(mm0, bs = \"ps\") "
+tag <- paste("te:", fmla)
+
+gam0 <- gam(as.formula(fmla),
+            weights = wts,
+            data = data1,
+            family = family0)
+
+gam0[["name0"]] <- tag
+gam0[["ftres"]] <- unique(ftres)
+gs[[tag]] <- gam0
+
+ltp.gamS(gam0, force0=force0)       # just to start with
+force0 <- FALSE
+
+summary(gam0)$r.sq
+
+x.plots[['final']] <- ltp.chart(gam0, tbl=data1)
 
 ## Write plots
 
 jpeg(filename=paste("gam0", "-%03d.jpeg", sep=""), 
      width=1024, height=768)
 
-## print(p1)
+plot(gam0)
 
-lapply(x.plots, print)
+scatter.smooth(x=1:length(d1), y=d1)    # see a scatter
+
+x0 <- lapply(x.plots, print)
 
 x0 <- lapply(dev.list(), function(x) dev.off(x))
 
-
+plot(gs[[1]])
--- gam00.R
+++ gam00.R
@@ -2,11 +2,41 @@
 ##
 ## Support for GAM.
 
+## Exponentially weighted moving average
+
+if (!exists("ewma")) {
+    require("zoo")
+
+    ewma <- function(x,lambda = 1, init = x[1]) {
+
+        rval<-filter(lambda * coredata(x),
+                     filter=(1-lambda),method="recursive",
+                     init=init)
+        rval<-zoo(coredata(rval),index(x))
+        rval
+    }
+}
+
+## parameter holder
+ltp <- list()
+
+## The weights to use within GAM.
+
 ## Convert year and month to a date - BoM
 data0 <- read.csv(x.args[1], stringsAsFactors=FALSE)
 x.dt0 <- data0[["dt0"]]
 x.dt0 <- sapply(x.dt0, function(x) paste(x, "-01", sep=""))
 data0[["dt1"]] <- as.Date(x.dt0, format="%Y-%m-%d")
+
+### Flat weighting 
+wts.flat <- rep(1, dim(data0)[1])
+
+### Exponentially weighted - no need to reverse
+wts.exp <- rev(ewma(c(1, rep(0, length(wts.flat) - 1)), lambda=1-0.95))
+wts.expmean <- wts.exp / mean(wts.exp)
+
+### Set a default
+wts <- wts.flat
 
 ## Make the date an EoM
 require("lubridate")
@@ -27,14 +57,35 @@ d0 <- data0[["sales0"]]
 d1 <- d0 - min(d0)
 
 ### Try and remove the growth
-scatter.smooth(x=1:length(d1), y=d1)    # see a scatter
+### this includes some FX
+p1 <- scatter.smooth(x=1:length(d1), y=d1)    # see a scatter
+
+x.plots[['scatter']] <- p1
+
+x0 <- loess.smooth(x=1:length(d1), y=d1, evaluation=length(d1))
+
+data0[["growth"]] <- x0$y
+                                        
 plot(density(d1))                       # strange distribution
 
-## Make a model
+## Make a model for linear growth
 d2 <- data.frame(sales1=d1,idx=1:length(d1))
-mdl0 <- lm(sales1 ~ idx, data=d2)
+
+mdl0 <- lm(sales1 ~ idx, weights=wts.flat, data=d2)
+
+d3 <- mdl0$fitted.values
+d3[[length(d3)]] - d3[[1]]
+
+## with weights
+mdl0 <- lm(sales1 ~ idx, weights=wts.exp, data=d2)
+
+d3 <- mdl0$fitted.values
+d3[[length(d3)]] - d3[[1]]
+
 
 data0[["sales1"]] <- d1 - mdl0$fitted.values
+
+data0[["growth"]] <- mdl0$fitted.values
 
 ## Make FX more explicit by inverting
 data0[["fx1"]] <- 1 / data0[["fx0"]]
@@ -48,38 +99,96 @@ s0 <- summary(data0[["sales0"]])
 
 ## Scale and centre
 
-data1 <- sapply(data0[, c("fx1", "sales", "sales0", "sales1", "weather1")], 
+data1 <- sapply(data0[, c("fx1", "sales", "sales0", "sales1", "weather1", "growth")], 
                 function(x) scale(x, scale=TRUE, center=TRUE))
 data1 <- as.data.frame(data1)
 colnames(data1) <- paste("s.", colnames(data1), sep="")
 
 data0 <- cbind(data0, data1)
 
-## Exponentially weighted moving average
+## Make up some feature sets
 
-if (!exists("ewma")) {
-    require("zoo")
+## The outcome variable
+ltp[['outcomen']] <- x.args[2]
 
-    ewma <- function(x,lambda = 1, init = x[1]) {
+ltp[['date']] <- c('dt1')
 
-        rval<-filter(lambda * coredata(x),
-                     filter=(1-lambda),method="recursive",
-                     init=init)
-        rval<-zoo(coredata(rval),index(x))
-        rval
-    }
+## The only valid inputs
+ltp[['inputn']] <- c('mm0', 'weather', 'fx1', 'dh0', 'growth')
+
+## The derived outputs can be disregarded
+ltp[['xclde']] <- setdiff(colnames(data0),
+                          union(ltp[['outcomen']], 
+                                union(ltp[['date']], ltp[['inputn']]) ) )
+
+ltp.paste0 <- function(x, bs0="ps") {
+    return(sprintf("s(%s, bs=\"%s\")", x, bs0))
 }
 
-## The weights to use within GAM.
+ltp.gamS <- function(gam1, zz="all.Rout", force0=FALSE) {
+    if (force0) {
+        unlink(zz, force=force0)
+        fl <- file(zz, "wt")
+    }
+    
+    x.gam <- gam1
+    zz <- file(zz, open = "at")
+    sink(zz)
+    cat("\n\n")
+    cat(sprintf("start: %s", x.gam$name), "\n")
+    ## what to look at: summary: EDF, p-values, R^2, GCV; AIC, magic
+    cat("summary: ")
+    print(summary(x.gam))
 
-### Flat weighting 
-wts.flat <- rep(1, dim(data0)[1])
+    cat("R^2: ", summary(x.gam)$r.sq, "; ")
 
-### Exponentially weighted - no need to reverse
-wts.exp <- ewma(c(1, rep(0, length(wts.flat) - 1)), lambda=1-0.90)
-wts.expmean <- wts.exp / mean(wts.exp)
+    cat("GCV: ", summary(x.gam)$sp.criterion, "; ")
 
-### Set a default
-wts <- wts.flat
+    cat("AIC: ", x.gam$aic, "; ")
 
-## Others
+    cat("BIC: ", BIC(x.gam))
+    cat("\n")
+
+    # sink(zz, type = "message")
+    ## revert output back to the console -- only then access the file!
+    ## sink(type = "message")
+    sink()
+    return(zz)
+}
+
+
+ltp.chart <- function(gam1, ctr0="unknown", nocoef=FALSE, tbl=data0,
+                      c0=c("value", "dt1"), name1="sales") {
+
+    ## Number of coefficients
+    if (!nocoef) {
+        l0 <- length(unique(gsub("\\.[0-9]+$", "", names(gam1$coefficients))))
+
+        layout(matrix(1:l0, nrow = 1))
+        plot(gam1)
+    }
+
+    x.df1 <- tbl
+    if (is.null(tbl)) {
+        x.df1 <- (gam1$model)[, 1, drop = FALSE]
+        x.df1[, c0[[2]] ] <- rownames(x.df1)
+        colnames(x.df1)[1] <- c0[1]
+    } else {
+        x.df1 <- tbl[, c0]
+    }
+    x.df1$type <- "Real"
+
+    x.df2 <- data.frame(value = gam1$fitted.values, 
+                        dt1 = x.df1[, c0[[2]] ] )
+    x.df2$type <- "Fitted"
+
+    datas <- rbind(x.df1, x.df2)
+
+    p1 <- ggplot(data = datas, aes(dt1, value, group = type, colour = type)) +
+        geom_line(size = 0.8) +
+        theme_bw() +
+        labs(x = "date", y = name1,
+             title = sprintf("Fit from GAM - %s", gam1[["name0"]]))
+
+    return(p1)
+}
