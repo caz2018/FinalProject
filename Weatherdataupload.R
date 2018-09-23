44ecb8a3298a1bb36e1069683b448f70e7e36819
Caroline <caroline.silvestre@live.com>
2018-09-23 01:21:43 +0100

    made a df I can work with

--- Weatherdataupload.R
+++ Weatherdataupload.R
@@ -3,28 +3,45 @@ library(readr)
 library(lubridate)
 library(ggplot2)
 library(forecast)
+library(zoo)
 
 WeatherData <- read_csv("WeatherDaily.csv")
 weatherdates <- as.Date(WeatherData$DATE, "%d/%m/%Y")
 #View(weatherdates)
 WeatherData$DATE <- weatherdates
+#remove NA's
 WeatherData$PRCP[is.na(WeatherData$PRCP)] <- 0
+
 
 CurrencyData <- read.csv("CurrencyExchangeDaily.csv")
 currencydates <- mdy(CurrencyData$Date)
-CurrencyData$Date <- currencydates 
+CurrencyData$Date <- currencydates
+#trading stops on weekends, so NA's will carry the latest value
+
+#removing data we don't need
+CurrencyData$Open <- NULL
+CurrencyData$High <- NULL
+CurrencyData$Low <- NULL
+CurrencyData$Change.. <- NULL
 
 
-#merging the 2 dataframes
-newdf <- merge(SalesData, WeatherData, by.x = "Date", by.y = "DATE")
+#merging weather and sales dataframes
+newdf <- merge(SalesData, WeatherData, by.x = "Date", by.y = "DATE", all.x = TRUE)
 summary(newdf)
-newdf <- merge(newdf, CurrencyData, by.x = "Date", by.y = "Date")
+#adding currency data
+newdf <- merge(newdf, CurrencyData, by.x = "Date", by.y = "Date", all.x = TRUE)
+summary(newdf)
+#dealing with NA's in currency data 
+newdf$Price <- na.locf(newdf$Price)
+#adding decomposition data
+newdf <- merge(newdf, dat_decomp, by.x = "Date", by.y = "Date", all.x = TRUE)
 summary(newdf)
 
-#standardizing and normalising the weather data
-newdf$PRCP <- normalize(newdf$PRCP, method="range", range = c(1,10))
+
+#standardizing and normalising the weather and currency data
+newdf$PRCP <- normalize(newdf$PRCP, method="range", range = c(0,10))
 newdf$TAVG <- normalize(newdf$TAVG, method="range", range = c(1,10))
-newdf$PRCP <- normalize(newdf$PRCP, method="range", range = c(1,10))
+newdf$Price <- normalize(newdf$Price, method="range", range = c(1,10))
 
 summary(newdf)
 
