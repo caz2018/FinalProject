94f8a4dec4a3a7897bf7c33013828b3cab105e11
Walter Eaves <walter.d.eaves@gmail.com>
2018-09-26 20:56:08 +0100

    gam0 and gam00

--- gam00.R
+++ gam00.R
@@ -1,3 +1,5 @@
+## weaves
+##
 ## Support for GAM.
 
 ## Exponentially weighted moving average
@@ -20,6 +22,11 @@ ltp <- list()
 
 ## The weights to use within GAM.
 
+## Convert year and month to a date - BoM
+data0 <- read.csv(x.args[1], stringsAsFactors=FALSE)
+x.dt0 <- data0[["dt0"]]
+x.dt0 <- sapply(x.dt0, function(x) paste(x, "-01", sep=""))
+data0[["dt1"]] <- as.Date(x.dt0, format="%Y-%m-%d")
 
 ### Flat weighting 
 wts.flat <- rep(1, dim(data0)[1])
@@ -31,6 +38,23 @@ wts.expmean <- wts.exp / mean(wts.exp)
 ### Set a default
 wts <- wts.flat
 
+## Make the date an EoM
+require("lubridate")
+data0[["dt1"]] <- data0[["dt1"]] + months(1) - days(1)
+### add the number of the month
+data0[["mm0"]] <- month(data0[["dt1"]])
+
+## Daylight hours for each month
+dh0 <- read.csv("src/dhm.csv")
+data0 <- merge(data0, dh0, by="mm0")
+
+## Order this
+data0 <- data0[ order(data0$dt1), ]
+
+## Add a log of sales
+data0[["sales0"]] <- log(data0[["sales"]])
+d0 <- data0[["sales0"]]
+d1 <- d0 - min(d0)
 
 ### Try and remove the growth
 ### this includes some FX
@@ -66,6 +90,9 @@ data0[["growth"]] <- mdl0$fitted.values
 ## Make FX more explicit by inverting
 data0[["fx1"]] <- 1 / data0[["fx0"]]
 
+## Make weather more explicit by inverting
+m0 <- max(data0[["weather"]])
+data0[["weather1"]] <- abs(data0[["weather"]] - m0)
 
 ## Remove some growth
 s0 <- summary(data0[["sales0"]])
